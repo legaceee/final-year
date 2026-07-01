dad396b6fea8dd0518844464678c5275c5727f5d
aman pandey <amanpandey2707@gmail.com>
2026-07-01 14:14:31 +0530

    init

---
+++ packages/db/src/index.ts
@@ -0,0 +1,19 @@
+import { PrismaClient } from "./generated/prisma/client.js";
+//Prisma Driver Adapter for Postgres
+import { PrismaPg } from "@prisma/adapter-pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+// Create a new Driver Adapter instance for PrismaPostgres
+console.log("DB URL inside db package:", process.env.DATABASE_URL);
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+});
+const globalForPrisma = globalThis as unknown as {
+  prisma?: PrismaClient;
+};
+export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter });
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
+export default prisma;
