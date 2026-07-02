c5b4886b92ec890b795a82fb8f3eed351750aa5f
aman pandey <amanpandey2707@gmail.com>
2026-07-02 21:22:35 +0530

    10 reviews extraction from amazon only

--- apps/extension/src/content.ts
+++ apps/extension/src/content.ts
@@ -1,1 +1,29 @@
 console.log("Amazon review loaded");
+const reviews = document.querySelectorAll('[data-hook="review"]');
+type rev={
+    title:string,
+    body:string,
+    rating:string,
+}
+const extractedReviews:rev[] = [];
+
+reviews.forEach((review) => {
+
+    const title =
+        review.querySelector('[data-hook="review-title"]')?.textContent?.trim() ?? "";
+
+    const body =
+        review.querySelector('[data-hook="review-body"]')?.textContent?.trim() ?? "";
+
+    const rating =
+        review.querySelector('[data-hook="review-star-rating"]')?.textContent?.trim() ?? "";
+
+    extractedReviews.push({
+        title,
+        rating,
+        body,
+    });
+
+});
+
+console.log(extractedReviews);
