449bbfacb3e2a56de9375e07c70b5c2e7bfa9f61
aman pandey <amanpandey2707@gmail.com>
2026-08-06 22:28:25 +0530

    preprocessing data

--- apps/python-ai/01_preprocess.py
+++ apps/python-ai/01_preprocess.py
@@ -4,3 +4,44 @@ INPUT_FILE = "../dataset/raw/final_labeled_fake_reviews.csv"
 OUTPUT_FILE = "../dataset/processed/reviews_clean.csv"
 
 df = pd.read_csv(INPUT_FILE)
+df = df[
+    [
+        "rating",
+        "title",
+        "text",
+        "helpful_vote",
+        "verified_purchase",
+        "label",
+    ]
+]
+df = df.dropna(subset=["title", "text"])
+df["title"] = df["title"].astype(str).str.strip()
+df["text"] = df["text"].astype(str).str.strip()
+df["review"] = (
+    "[TITLE] "
+    + df["title"]
+    + " [BODY] "
+    + df["text"]
+)
+df = df.drop_duplicates(subset=["review"])
+df = df[
+    df["review"].str.split().str.len() >= 5
+]
+df = df.reset_index(drop=True)
+df.to_csv(
+    OUTPUT_FILE,
+    index=False
+)
+print("=" * 40)
+print("Dataset Cleaning Complete")
+print("=" * 40)
+
+print(f"Total Reviews : {len(df)}")
+
+print("\nLabel Distribution")
+
+print(df["label"].value_counts())
+
+print("\nSaved to:")
+
+print(OUTPUT_FILE)
