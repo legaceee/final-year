8a14ac7ce68916c58b5ee12cb9b703602706c4a6
aman pandey <amanpandey2707@gmail.com>
2026-08-06 21:06:03 +0530

    basic data training

---
+++ apps/python-ai/inspect_dataset.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+df=pd.read_csv("../dataset/raw/final_labeled_fake_reviews.csv")
+
+print(df.head())
+print("\nshape")
+print(df.shape)
+print("\nColumns")
+print("\nMissing values")
+print(df.isnull().sum())
+
+print("\nData types")
+print(df.dtypes)
+print("\nLabel Distribution")
+print(df.iloc[:, -1].value_counts())
