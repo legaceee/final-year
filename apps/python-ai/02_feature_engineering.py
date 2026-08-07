a5308fd634530d3e4b64977dfc974e87376c7627
aman pandey <amanpandey2707@gmail.com>
2026-08-08 01:01:12 +0530

    pipeline 2

--- apps/python-ai/02_feature_engineering.py
+++ apps/python-ai/02_feature_engineering.py
@@ -2,4 +2,7 @@ import pandas as pd
 
 df=pd.read_csv("../dataset/processed/reviews_clean.csv")
 df["review_length"]=df["review"].str.len()
-print(df["review_length"])
+print(df[["review", "review_length"]].head())
+print(df.columns.tolist())
+df["word_count"]=df["review"].str.split().str.len()
+print(df["word_count"].head())
