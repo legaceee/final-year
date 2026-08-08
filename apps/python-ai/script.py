8fb6089924c73ad679182fe65227f179b9c68eca
aman pandey <amanpandey2707@gmail.com>
2026-08-08 21:04:48 +0530

    finding why user_review_burst has same val

--- apps/python-ai/script.py
+++ apps/python-ai/script.py
@@ -2,3 +2,9 @@ import pandas as pd
 df=pd.read_csv("../dataset/raw/final_labeled_fake_reviews.csv")
 print("user review burst:")
 print(df["user_review_burst"].describe())
+
+print("\n99999 count:")
+print((df["user_review_burst"] == 99999).sum())
+
+print("\nOther values:")
+print(df.loc[df["user_review_burst"]!=99999,["user_id","timestamp","user_timestamp","user_review_burst","label"]].head(20))
