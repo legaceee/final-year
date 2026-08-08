ae0e21e524fb071dc55a61104f8f67998bac6a69
aman pandey <amanpandey2707@gmail.com>
2026-08-08 20:08:22 +0530

    02_feature_engineering started

--- apps/python-ai/02_feature_engineering.py
+++ apps/python-ai/02_feature_engineering.py
@@ -4,6 +4,9 @@ def count_Sentences(review):
      if count == 0:
         return 1
      return count
+def count_exclamation(review):
+     count=review.count("!")
+     return count
 df=pd.read_csv("../dataset/processed/reviews_clean.csv")
 df["review_length"]=df["review"].str.len()
 print(df[["review", "review_length"]].head())
@@ -11,3 +14,16 @@ print(df.columns.tolist())
 df["word_count"]=df["review"].str.split().str.len()
 print(df["word_count"].head())
 df["sentence_count"]=df["review"].apply(count_Sentences)
+df["excalamation count"]=df["review"].str.count("!")
+print(df["exclamation count"])
+
+# review_length
+# word_count
+# sentence_count
+# to do 
+# average_word_length
+# exclamation_count
+# question_count
+# uppercase_ratio
+# punctuation_ratio
+# lexical_diversity
