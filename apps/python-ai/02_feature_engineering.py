35f6f9a6d9f9115f5cd184c4e605798234c8a565
aman pandey <amanpandey2707@gmail.com>
2026-08-08 19:38:21 +0530

    02_feature_engineering started

--- apps/python-ai/02_feature_engineering.py
+++ apps/python-ai/02_feature_engineering.py
@@ -1,9 +1,13 @@
 import pandas as pd
-
+def count_Sentences(review):
+     count = review.count(".") + review.count("!") + review.count("?")
+     if count == 0:
+        return 1
+     return count
 df=pd.read_csv("../dataset/processed/reviews_clean.csv")
 df["review_length"]=df["review"].str.len()
 print(df[["review", "review_length"]].head())
 print(df.columns.tolist())
 df["word_count"]=df["review"].str.split().str.len()
 print(df["word_count"].head())
-df["sentence_count"]=df["review"].str.split(".").str.len
+df["sentence_count"]=df["review"].apply(count_Sentences)
